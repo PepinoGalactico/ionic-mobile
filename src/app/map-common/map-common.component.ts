6633bff4bbc282fe74e3cc5dba233ccaa5d9f82b
ununpentium <66434813+PepinoGalactico@users.noreply.github.com>
2024-09-08 19:52:04 -0300

    maps design

--- src/app/map-common/map-common.component.ts
+++ src/app/map-common/map-common.component.ts
@@ -13,12 +13,30 @@ export class MapCommonComponent implements OnInit {
 
   ngOnInit(): void {
     mapboxgl.accessToken = 'pk.eyJ1IjoicGVwaW5vZGV2IiwiYSI6ImNtMGNyeHUyMjA1eGIyanByaHU5aXU4OGEifQ.CfbNZGUoxOobO2MILtDIfA';
+
+    const currentHour = new Date().getHours();
+    let lightPreset = 'day';
+
+    if (currentHour >= 6 && currentHour < 8) {
+      lightPreset = 'dawn';
+    } else if (currentHour >= 9 && currentHour < 18) {
+      lightPreset = 'day';
+    } else if (currentHour >= 18 && currentHour < 20) {
+      lightPreset = 'dusk';
+    } else {
+      lightPreset = 'night';
+    }
+
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/standard',
       center: [-70.61489622556584, -33.43296948792158],
       zoom: 16,
       attributionControl: false
+    });
+
+    map.on('style.load', () => {
+      map.setConfigProperty('basemap', 'lightPreset', lightPreset);
     });
 
     map.on('render', function() {
